b362a53b4d4c7d368faaecad6bd35fea85140c81
Alex Ayala (S-Toad) <AlexAyala9975@hotmail.com>
2016-08-27 13:59:44 -0700

    Update main ino to use test motor handler code

--- SumoProject.ino
+++ SumoProject.ino
@@ -1,21 +1,20 @@
-#include "Arduino.h"
-#include "UltraSonicHandler.h"
-//The setup function is called once at startup of the sketch
-
-const int SERIAL_DATA_TRANSMISSION_SPEED = 9600;
-long cm;
-// First Ultra Sonic Sensor is defined with the trig pin 3 and 5. This allows us to easily create new instances of different sensors without having to write the same code over and over.
-UltraSonicHandler ultraSonicSensor1 = UltraSonicHandler(11, 12);
+#include "MotorHandler.h"
+MotorHandler motorTest(8, 9, 10);
 
 void setup() {
-	Serial.begin(SERIAL_DATA_TRANSMISSION_SPEED);
+	Serial.begin(9600);
 	Serial.print("STARTING");
 }
 
-// The loop function is called in an endless
 void loop() {
-  long cm = ultraSonicSensor1.getAveragedDistance(10);
-  Serial.print(cm);
-  Serial.println();
-	delay(250);
+	motorTest.setForward();
+	motorTest.setSpeed(100);
+	delay(5000);
+	motorTest.setReverse();
+	delay(5000);
+	motorTest.setSpeed(50);
+	delay(5000);
+	motorTest.stop();
+	delay(5000);
 }
+
